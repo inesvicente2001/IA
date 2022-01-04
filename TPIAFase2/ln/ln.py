d8c3336dded63a2c16f496744a71188f0cf131e1
MiguelAMM42 <a93280@alunos.uminho.pt>
2022-01-04 23:07:27 +0000

    Bfs

--- TPIAFase2/ln/ln.py
+++ TPIAFase2/ln/ln.py
@@ -10,6 +10,9 @@ import pandas as pd
 import random
 import tkinter as tk
 import networkx
+from queue import Queue
+
+####################################################################################################
 
 
 def dfs_aux(g, start, target, path, visited = set()):
@@ -33,7 +36,11 @@ def dfs(target):
     #load_search_graph(path)
     return path
     
-    
+
+
+####################################################################################################
+
+
 def load_search_graph(path):
     prefs = create_prefs()
     print(path)
@@ -41,6 +48,8 @@ def load_search_graph(path):
     
     
     for (i, node) in enumerate(path[1:]):
+        if node==697: 
+            print("oof")
         prefs["vertex_color"][node] = "blue"
         prefs["vertex_size"][node] = 20
         edge_id = g.get_eid(path[i], path[i+1], error = False)
@@ -51,3 +60,168 @@ def load_search_graph(path):
     prefs["vertex_color"][path[len(path)-1]] = "yellow"
     print(total_cost)
     plot(g, **prefs)
+
+
+
+####################################################################################################
+
+def bfs_aux(graph, start_node, target_node):
+    # Set of visited nodes to prevent loops
+    visited = set()
+    queue = Queue()
+
+    # Add the start_node to the queue and visited list
+    queue.put(start_node)
+    visited.add(start_node)
+    
+    # start_node
+    parent = dict()
+    parent[start_node] = None
+
+    # Perform step 3
+    path_found = False
+    while not queue.empty():
+        current_node = queue.get()
+        if current_node == target_node:
+            path_found = True
+            break
+
+        for next_node in g.neighbors(current_node):
+            if next_node not in visited:
+                queue.put(next_node)
+                parent[next_node] = current_node
+                visited.add(next_node)
+                
+    # Path reconstruction
+    path = []
+    if path_found:
+        path.append(target_node)
+        while parent[target_node] is not None:
+            path.append(parent[target_node]) 
+            target_node = parent[target_node]
+        path.reverse()
+    return path
+
+
+
+def bfs(target):
+    path = bfs_aux(g, g.vs["rua"].index("Green Distribution"), g.vs["rua"].index(target))
+    print(path)
+    #load_search_graph(path)
+    return path
+
+
+######################################################################################################
+##
+### heuristic function with equal values for all nodes
+##def h(graph, n):
+##    H = {
+##        'A': 1,
+##        'B': 1,
+##        'C': 1,
+##        'D': 1
+##    
+##    return H[n]
+##
+##
+##def a_star_algorithm(graph, start_node, stop_node):
+##        # open_list is a list of nodes which have been visited, but who's neighbors
+##        # haven't all been inspected, starts off with the start node
+##        # closed_list is a list of nodes which have been visited
+##        # and who's neighbors have been inspected
+##        open_list = set([start_node])
+##        closed_list = set([])
+##
+##        # g contains current distances from start_node to all other nodes
+##        # the default value (if it's not found in the map) is +infinity
+##        g = {}
+##
+##        g[start_node] = 0
+##
+##        # parents contains an adjacency map of all nodes
+##        parents = {}
+##        parents[start_node] = start_node
+##
+##        while len(open_list) > 0:
+##            n = None
+##
+##            # find a node with the lowest value of f() - evaluation function
+##            for v in open_list:
+##                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+##                    n = v;
+##
+##            if n == None:
+##                print('Path does not exist!')
+##                return None
+##
+##            # if the current node is the stop_node
+##            # then we begin reconstructin the path from it to the start_node
+##            if n == stop_node:
+##                reconst_path = []
+##
+##                while parents[n] != n:
+##                    reconst_path.append(n)
+##                    n = parents[n]
+##
+##                reconst_path.append(start_node)
+##
+##                reconst_path.reverse()
+##
+##                print('Path found: {}'.format(reconst_path))
+##                return reconst_path
+##
+##            # for all neighbors of the current node do
+##            for (m, weight) in self.get_neighbors(n):
+##                # if the current node isn't in both open_list and closed_list
+##                # add it to open_list and note n as it's parent
+##                if m not in open_list and m not in closed_list:
+##                    open_list.add(m)
+##                    parents[m] = n
+##                    g[m] = g[n] + weight
+##
+##                # otherwise, check if it's quicker to first visit n, then m
+##                # and if it is, update parent data and g data
+##                # and if the node was in the closed_list, move it to open_list
+##                else:
+##                    if g[m] > g[n] + weight:
+##                        g[m] = g[n] + weight
+##                        parents[m] = n
+##
+##                        if m in closed_list:
+##                            closed_list.remove(m)
+##                            open_list.add(m)
+##
+##            # remove n from the open_list, and add it to closed_list
+##            # because all of his neighbors were inspected
+##            open_list.remove(n)
+##            closed_list.add(n)
+##
+##        print('Path does not exist!')
+##        return None
+##
+##
+##
+##
+##
+
+
+
+####################################################################################################
+
+
+
+#name = "Rua de Santa Luzia"
+#name = "Rua do Monte Lombo" #onde dá bug
+name = "Rua Mira Rio"
+#path= dfs(name)
+#load_search_graph(path)
+
+#name = "Rua de Santa Luzia"
+##name = "Rua do Monte Lombo" #onde dá bug
+path= bfs(name)
+load_search_graph(path)
+
+
+
+#print(g)
+#print(graph_as_adjacency_list(g))
